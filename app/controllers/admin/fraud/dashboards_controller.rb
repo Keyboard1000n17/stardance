76b0c88d139d54874a7b2a3407d03b0225cbf73c
Neon <neon@saahild.com>
2026-07-02 23:38:02 -0400

    update: add the cool think the shipwrights have but for fraud dept :)
    Signed-off-by: Neon <neon@saahild.com>

--- app/controllers/admin/fraud/dashboards_controller.rb
+++ app/controllers/admin/fraud/dashboards_controller.rb
@@ -3,5 +3,7 @@ class Admin::Fraud::DashboardsController < Admin::ApplicationController
     authorize :fraud_dashboard
     @pending_orders_count = ShopOrder.where(aasm_state: %w[pending awaiting_verification awaiting_verification_call awaiting_periodical_fulfillment on_hold]).count
     @pending_fraud_reports_count = ::Project::Report.pending.where(reason: "fraud").count
+
+    @order_stats = ShopOrder.dashboard_stats
   end
 end
